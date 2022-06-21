09437ccaa7d165ec1a093f91eb79b617bedb1313
Bishnu <bishnuthapako@gmail.com>
2022-06-21 17:55:26 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,7 @@ function App() {
           <Question
             question={currentQuestion}
             onAnswered={handleQuestionAnswered}
+
           />
         ) : (
           <>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,44 @@
-import React, { useState } from "react";
+import { cleanup } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
+
+
   const [timeRemaining, setTimeRemaining] = useState(10);
+  // const [count, setCount]=useState(0)
+
 
   // add useEffect code
 
+  useEffect(()=>{
+   
+   const timeCount = setInterval(()=>{setTimeRemaining(time=>time-1)
+      if(timeRemaining === 0) {
+        setTimeRemaining(10)
+        onAnswered(false)
+
+      }
+  }, 1000)
+  
+     return function cleanup(){
+        clearTimeout(timeCount)
+       
+     }
+  }, [timeRemaining] )
+
   function handleAnswer(isCorrect) {
+    if(timeRemaining === 0){
+      return  handleAnswer()
+      onAnswered()
+
+    }
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
   const { id, prompt, answers, correctIndex } = question;
+
+  console.log(prompt)
 
   return (
     <>
